3a9446327c05de8446d7423854bbb03d0b243968
David Tolnay <dtolnay@gmail.com>
2026-01-30 14:13:55 -0800

    Release 1.0.18

--- src/lib.rs
+++ src/lib.rs
@@ -41,7 +41,7 @@
 //! ![performance](https://raw.githubusercontent.com/dtolnay/zmij/master/dtoa-benchmark.png)
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/zmij/1.0.17")]
+#![doc(html_root_url = "https://docs.rs/zmij/1.0.18")]
 #![deny(unsafe_op_in_unsafe_fn)]
 #![allow(non_camel_case_types, non_snake_case)]
 #![allow(
